57fe77d54d0b2270bf0c4f377e9f3f9ee84aa632
Hung <22645171.hung@student.iuh.edu.vn>
2026-07-10 11:11:51 +0700

    feat: implement ownership control in Counter contract and update ABI

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,11 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract Counter {
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Counter is Ownable {
     uint public count;
 
-    function increment() public {
+    event CountIncremented(uint256 newCount);
+
+    constructor() Ownable(msg.sender) {}
+
+    function increment() public onlyOwner {
         count += 1;
+        emit CountIncremented(count);
     }
 
     function getCount() public view returns (uint) {
